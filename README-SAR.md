ce169315d0f77eeb6bf8a78856180338bf01a3a5
bstraehle <bstraehle@gmail.com>
2022-02-25 18:47:30 -0800

    first commit

---
+++ README-SAR.md
@@ -0,0 +1,15 @@
+# Serverless nanoservice using SQS and Lambda  
+
+Start up:  
+
+- Deploy application, specifying queue name  
+
+Test:  
+
+- Go to SQS, queues, queue, and send message  
+- Go to DynamoDB, tables, table, and explore table items  
+
+Tear down:  
+
+- Go to CloudFormation and delete application stack  
+- Go to CloudWatch, log groups, log group and delete  
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const DDB = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const TABLE_NAME = process.env.TABLE_NAME;
+
+exports.handler = (event) => {
+    const message = event.Records[0].body;
+
+    console.log('Message:', msg);
+
+    var params = {
+        TableName: TABLE_NAME,
+        Item: {
+            'id' : {S: msg}
+        }
+    };
+
+    DDB.putItem(params, function(err) {
+        if (err) {
+            console.log("Error:", err);
+        } else {
+            console.log("Success");
+        }
+    });
+};
