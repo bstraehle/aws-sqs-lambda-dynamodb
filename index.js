cb88641d817cb36b35f2657b77002d1175c17596
bstraehle <bstraehle@gmail.com>
2022-02-26 14:53:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const DDB = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const TABLE_NAME = process.env.TABLE_NAME;
 
 exports.handler = (event) => {
-    const msg = event.Records[0].body;
+    var msg = event.Records[0].body;
 
     console.log('Message:', msg);
 
